0defde9fe6f556becdcd877cd3769f2ee41d0dce
admin-on-win10 <shashwatjaiswal14@gmail.com>
2021-10-02 18:06:17 +0530

    refi 6pm saturday

---
+++ scripts/refi.js
@@ -0,0 +1,27 @@
+$(".ammortization-table-container")
+.slideUp()
+.css('opacity','0');
+
+$(".ammortization-btn").on("click", () => {
+   changeText()
+   // animateTable(changeText())
+   $(".ammortization-table-container")
+   .slideToggle(700);
+   // .animate({'opacity':1},400)
+   animateTable($('#show-table').text())
+})
+
+function changeText() {
+   let x = $("#show-table").text() == 'Show' ? 'Hide' : 'Show';
+   $("#show-table").text(x);
+}
+
+function animateTable(val){
+   console.log(val)
+   if (val=='Hide') {
+      $('.ammortization-table-container').animate({'opacity':1},400)
+   }else{
+      $('.ammortization-table-container').animate({'opacity':0},400)
+   }
+}
+
