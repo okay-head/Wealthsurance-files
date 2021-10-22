4a554bbdd0b7d48fcf39d03f6db167b5a5fab8c4
admin-on-win10 <shashwatjaiswal14@gmail.com>
2021-10-22 11:37:22 +0530

    zip code added to mortg and refi

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -25,10 +25,8 @@ $('#graph-btn,#table-btn').on('click', () => {
 
 $(".ammortization-btn").on("click", () => {
    changeText()
-   // animateTable(changeText())
    $(".ammortization-table-container")
    .slideToggle(700);
-   // .animate({'opacity':1},400)
    animateTable($('#show-table').text())
 })
 
@@ -53,11 +51,6 @@ function next() {
          let update = new Promise((resolve) => {
             resolve(updateLoader(1, 1));
          });
-         //----- @prasad -----
-         function pushToDatabase() {
-            console.log(mortg);
-         }
-         // ------------------
          return update;
       }
       loaderPromise().then(() => {
@@ -75,7 +68,7 @@ function pgLocalStorage() {
    //check for existing localStorage on page load
    if (localStorage.getItem("mortgpg1") != null) {
       mortg.page1 = JSON.parse(localStorage.getItem("mortgpg1")); 
-      for (let i = 0; i < 11; i++) {
+      for (let i = 0; i < 12; i++) {
          $('#mortgpg1e'+(i+1)).val(mortg.page1[i]);
       }
    }
@@ -89,7 +82,7 @@ function pgLocalStorage() {
 
       $(".next-btn").one("click", () => {
          mortg.page1 = [];
-         for (let i = 0; i < 11; i++) {
+         for (let i = 0; i < 12; i++) {
             mortg.page1.push($("#mortgpg1e"+(i+1)).val());
          }
          localStorage.setItem("mortgpg1", JSON.stringify(mortg.page1));
@@ -107,9 +100,11 @@ function next() {
          return update;
       }
       loaderPromise().then(() => {
+         // pushToDatabase1(mortg)
          setTimeout(() => {
             console.log(mortg)
-            // window.open("mortgage_result.html", "_self");
+
+            window.open("mortgage_result.html", "_self");
          }, 1410);
       });
    });
@@ -117,19 +112,10 @@ function next() {
 
 function reCalculate() {
    $(".re-calc-btn").on("click", () => {
-      /*      - - -
-      
-      some error checking
 
-              - - - 
-      */
      storeRecalculate();
 
-   // ----------  
-   // @prasad
-   // Push to database and calculate results
      console.log(mortg.results)
-   //   ----------
    
    });
 }
@@ -154,4 +140,128 @@ function storeRecalculate() {
    mortg.results.push(obj);
 }
 
+
+function pushToDatabase1(mortg) {
+   createSessionId();
+   let [a, b, c, d, e, f, g] = mortg.page1;
+
+   // let url_string = `http://wealthsurance.com/calculators/?calculator=college&session_id=${session_id}&ip_address=${ip}&child_name=${a}&curr_age=${b}&college_year=${c}&year_in_college=${d}&coll_expense=${f}&to_fund_percent=${e}&to_fund_amnt=${
+   //    ((f * e) / 100).toFixed(2)
+   // }&inv_rate=${g}`;
+
+   let url_string = `http://wealthsurance.com/calculators/?calculator=mortgage&session_id=prasad123&ip_address=139.5.30.66&data={"price" : 500000,"amount" : 450000 ,"down" : 50000,"interest" : 3.5,"duration" : 30,"hoaDue" : 2400,"propTax" : 15000,"propIns" : 1500,"stPmi" : 1,"fnPmi" : 20}&type=1`
+
+   $.ajax({
+      type: "POST",
+      url: url_string,
+
+      success: (x) => {
+         console.log(x)
+      //    let result = JSON.parse(x);
+      //    console.log(result.amount);
+      //    console.log(url_string);
+
+      //    if (result.success) {
+      //       localStorage.setItem("mortg_result", JSON.stringify(result.amount));
+      //    } else {
+      //       console.log(result + "request not successful");
+      //    }
+      // },
+      // error: (error) => {
+      //    console.log(error);
+      },
+   });
+}
+
+function pushToDatabase2(mortg) {
+   createSessionId()
+   let [{ 
+      'name': a,
+      'current_age': b,
+      'age_entering_mortg': c,
+      'mortg_years': d,
+      'annual_mortg_expenses': e,
+      'portion_funded%': f,
+      'investment_rate': g,
+   }] = mortg.results
+
+   let url_string = `http://wealthsurance.com/calculators/?calculator=college&session_id=${session_id}&ip_address=${ip}&child_name=${a}&curr_age=${b}&college_year=${c}&year_in_college=${d}&coll_expense=${e}&to_fund_percent=${f}&to_fund_amnt=${
+      ((f * e) / 100).toFixed(2)
+   }&inv_rate=${g}`;
+
+   $.ajax({
+      type: "POST",
+      url: url_string,
+
+      success: (x) => {
+         let result = JSON.parse(x);
+         if (result.success) {
+            updateResult(2,result.amount)
+         } else {
+            console.log(result + "request not successful");
+         }
+      },
+      error: (error) => {
+         console.log(error);
+      },
+   });
+
+}
+
+function updateResult(x, y = undefined) {
+   switch (x) {
+      case 1:
+         $(".calculated-result").text(
+            "$" + JSON.parse(localStorage.getItem("mortg_result"))
+         );
+         break;
+      case 2:
+         $(".calculated-result").text("$" + y);
+         break;
+   }
+}
+
+function updatePlaceholders(x) {
+   switch (x) {
+      case 1:
+         const z = JSON.parse(localStorage.getItem("mortgpg1"));
+         for (let i = 0; i < 4; i++) {
+            $("#mortgpg1resulte" + (i + 1)).attr("placeholder", z[i]);
+         }
+         $("#mortgpg1resulte5").attr("placeholder", z[5]);
+         $("#mortgpg1resulte6").attr("placeholder", z[4]);
+         $("#mortgpg1resulte7").attr(
+            "placeholder",
+            z[5] * (z[4] / 100).toFixed(2)
+         );
+         $("#mortgpg1resulte8").attr("placeholder",z[6])
+
+         break;
+
+      //recalculate
+      case 2:
+         [
+            {
+               "name": a[0],
+               "current_age": a[1],
+               "age_entering_mortg": a[2],
+               "mortg_years": a[3],
+               "annual_mortg_expenses": a[4],
+               "portion_funded%": a[5],
+               "investment_rate":a[6],
+            },
+         ] = mortg.results;
+
+         for (let i = 0; i < 6; i++) {
+            $("#mortgpg1resulte" + (i + 1)).attr("placeholder", a[i]);
+         }
+
+         let calc = a[5] * (a[4] / 100).toFixed(2);
+         $("#mortgpg1resulte7").attr("placeholder", calc);
+         $("#mortgpg1resulte7").val(calc);
+
+         $("#mortgpg1resulte8").attr("placeholder", a[6]);
+         break;
+   }
+}
 
